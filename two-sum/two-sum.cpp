91035d99b6e857835254d7f48c95c15ba164e00b
Ayush Mazumdar <ayush.mz.2912@gmail.com>
2021-09-15 02:45:56 +0530

    Time: 17 ms (45.11%), Space: 10.9 MB (39.85%) - LeetHub

---
+++ two-sum/two-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &numbers, int target)
+    {
+    //Key is the number and value is its index in the vector.
+	unordered_map<int, int> hash;
+	vector<int> result;
+	for (int i = 0; i < numbers.size(); i++) {
+		int numberToFind = target - numbers[i];
+        
+		if (hash.find(numberToFind) != hash.end()) 
+        {   
+            //we found the required no in the map, push the answers into the ans array
+			result.push_back(hash[numberToFind]);
+			result.push_back(i);			
+			return result;
+		}
+
+		hash[numbers[i]] = i;
+	}
+	return result;
+}
+};
