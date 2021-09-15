c14d0734d3ec187e5252139cd43a1db70a6f7c3c
Ayush Mazumdar <ayush.mz.2912@gmail.com>
2021-09-15 19:18:27 +0530

    Time: 108 ms (59.20%), Space: 61.2 MB (59.22%) - LeetHub

--- find-the-duplicate-number/find-the-duplicate-number.cpp
+++ find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int ans;
-        unordered_map<int, int> mp;
-        for(int i = 0; i < nums.size(); i++){
-            mp[nums[i]]++;
-        }
         
-        for(auto it = mp.begin(); it != mp.end(); it++){
-            if(it->second > 1){
-                ans = it->first;
-            }
-        }
+        //defining the fast and slow pointers
+        int fast = nums[0];
+        int slow = nums[0];
         
-        return ans;
+        //create the cycle, create first coincide
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(fast != slow);
+        
+        
+        //move the fast ptr to the first element after coincide
+        fast = nums[0];
+        
+        //check for 2nd coincide, move both by 1 step
+        while(fast != slow){
+            slow = nums[slow];
+            fast = nums[fast];
+        };
+        
+        //return the slow ptr or fast ptr (ans = coincided node)
+        
+        return fast;
     }
 };
