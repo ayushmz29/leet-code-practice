055659062132308ec2f77a0413faeabfd6d220e4
Ayush Mazumdar <ayush.mz.2912@gmail.com>
2021-09-16 12:10:45 +0530

    Time: 5 ms (49.70%), Space: 10.8 MB (7.11%) - LeetHub

--- intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -2,26 +2,26 @@ class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
         
-        
+        //we need to create the hash-map for shorter array
         if(nums1.size() > nums2.size()) 
             return intersect(nums2, nums1);
         
-        vector<int> ret;
+        vector<int> ans;
         
         unordered_map<int,int> map1;
         
-        for(int num:nums1){
+        for(int num : nums1){
             map1[num]++;
         }
             
         
-        for(int num:nums2) {
-            if(map1[num]>0) {
-                ret.push_back(num);
+        for(int num : nums2) {
+            if(map1[num] > 0) {
                 map1[num]--;
+                ans.push_back(num);
             }
         }
-        return ret;
+        return ans;
         
     }
 };
